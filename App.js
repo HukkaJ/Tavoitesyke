ecb62633bcff3e53ac270fa7efa6d0fcdfb96363
Joona Mehtomaa <joona.mehtomaa@gmail.com>
2023-09-04 20:13:50 +0300

    Viikkotehtävä 1

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
-export default function App() {
+
+ 
+
+const App = () => {
+  const [age, setAge] = useState('');
+  const [targetHeartRate, setTargetHeartRate] = useState('');
+
+  const calcHeartRate = (newAge) => {
+    const ageNumber = parseInt(newAge, 10);
+    const maxHR = 220 - ageNumber;
+    const targetHRmin = Math.round(maxHR * 0.6);
+    const targetHRmax = Math.round(maxHR * 0.8);
+
+    setTargetHeartRate(`${targetHRmin}-${targetHRmax}`);
+  };
+
+
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Age :</Text>
+      <TextInput
+        placeholder="How old are you?"
+        keyboardType="numeric"
+        value={age}
+        onChangeText={(text) => {
+          setAge(text);
+          calcHeartRate(text);
+        }}
+      />
+      <Text>{targetHeartRate}</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +44,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
